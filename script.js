79af4fadcfc4f904df0338a08e238c9f12927777
Piotr Kielak <p.kielak@outlook.com>
2020-02-24 11:28:23 +0100

    buggy movement

--- script.js
+++ script.js
@@ -6,47 +6,6 @@ const scale = 20;
 const rows = canvas.height / scale;
 const columns = canvas.witdth / scale;
 
-// ctx.fillStyle = 'rgb(45,140,32)'
-// ctx.fillRect(0,0,scale*2,scale*3);
-
-// window.setTimeout(() => ctx.clearRect(0,0,scale*2,scale*3), 2000);
-
-// newRectangleBtn.addEventListener('click',()=>{
-//     ctx.clearRect(0,0,800,800);
-
-//     const randomX = Math.floor(Math.random()*40);
-//     const randomY = Math.floor(Math.random()*40);
-//     console.log(scale*randomX,scale*randomY);
-
-//     const randomHeight = Math.floor(Math.random()*5+1);
-//     const randomWidth = Math.floor(Math.random()*5+1);
-//     console.log(scale*randomWidth,randomHeight);
-
-//     ctx.fillStyle = 'rgb(45,140,32)'
-//     ctx.fillRect(scale*randomX,scale*randomY,scale*randomWidth,scale*randomHeight);
-// });
-
-// function createRectangle(){
-//     ctx.clearRect(0,0,800,800);
-
-//     const randomX = Math.floor((Math.random()*40+1)-1);
-//     const randomY = Math.floor((Math.random()*40+1)-1);
-
-//     const randomHeight = Math.floor(Math.random()*5+1);
-//     const randomWidth = Math.floor(Math.random()*5+1);
-    
-//     ctx.fillStyle = 'rgb(45,140,32)'
-//     ctx.fillRect(scale*randomX,scale*randomY,scale*randomWidth,scale*randomHeight);
-// }
-
-// const rectangleInterval = window.setInterval(createRectangle, 1000);
-
-// function stopInterval(){
-//     clearInterval(rectangleInterval);
-// }
-
-// stopRectangleInterval.addEventListener('click',stopInterval);
-
 class Snake {
     constructor(startX,startY){
         this.witdth = scale;
@@ -61,7 +20,19 @@ class Snake {
         ctx.fillRect(this.startX,this.startY,this.witdth,this.height);
     }
     move(){
-        this.startX += this.speed;
+        // this.startX += this.speed;
+
+        document.addEventListener('keydown',e => {
+            if(e.code === 'ArrowLeft'){
+                console.log('strzałka w lewo');
+                this.startX -= this.speed;
+            }
+
+            if(e.code === 'ArrowRight'){
+                console.log('strzałka w prawo');
+                this.startX += this.speed;
+            }
+        });
     }
 }
 
@@ -78,3 +49,7 @@ function animationFrame(){
 }
 
 const refreshFrame = setInterval(animationFrame,250);
+
+// document.addEventListener('keydown',e => {
+//     console.log(e.code);
+// });
