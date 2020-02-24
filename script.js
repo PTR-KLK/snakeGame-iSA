d2bf146775807fecb342c323472eaa1c06b9f2f2
Piotr Kielak <p.kielak@outlook.com>
2020-02-24 12:24:55 +0100

    fix snake movement

--- script.js
+++ script.js
@@ -10,29 +10,42 @@ class Snake {
     constructor(startX,startY){
         this.witdth = scale;
         this.height = scale;
-        this.speed = scale;
-        this.startX = startX;
-        this.startY = startY;
+        this.speed = scale/2;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.positionX = startX;
+        this.positionY = startY;
     }
 
     init(){
         ctx.fillStyle = 'rgb(45,140,32)'
-        ctx.fillRect(this.startX,this.startY,this.witdth,this.height);
+        ctx.fillRect(this.positionX,this.positionY,this.witdth,this.height);
     }
-    move(){
-        // this.startX += this.speed;
+    update(){
+        this.positionX += this.speedX;
+        this.positionY += this.speedY;
+    }
 
-        document.addEventListener('keydown',e => {
-            if(e.code === 'ArrowLeft'){
-                console.log('strzałka w lewo');
-                this.startX -= this.speed;
-            }
+    // move(){
+    //     this.positionX += this.speed;
+    // }
 
-            if(e.code === 'ArrowRight'){
-                console.log('strzałka w prawo');
-                this.startX += this.speed;
-            }
-        });
+    moveLeft(){
+        this.speedX -= this.speed;
+        this.speedY = 0;
+    }
+
+    moveRight(){
+        this.speedX += this.speed;
+        this.speedY = 0;
+    }
+    moveDown(){
+        this.speedY += this.speed;
+        this.speedX = 0;
+    }
+    moveUp(){
+        this.speedY -= this.speed;
+        this.speedX = 0;
     }
 }
 
@@ -45,11 +58,28 @@ function animationFrame(){
     ctx.clearRect(0,0,800,800);
     
     snake.init();
-    snake.move();
+    snake.update();
+    // snake.move();
 }
 
-const refreshFrame = setInterval(animationFrame,250);
+const refreshFrame = setInterval(animationFrame,100);
 
-// document.addEventListener('keydown',e => {
-//     console.log(e.code);
-// });
+document.addEventListener('keydown',e => {
+    if(e.code === 'ArrowLeft'){
+        console.log('strzałka w lewo');
+        snake.moveLeft();
+    }
+
+    if(e.code === 'ArrowRight'){
+        console.log('strzałka w prawo');
+        snake.moveRight();
+    }
+    if(e.code === 'ArrowUp'){
+        console.log('strzałka w góre');
+        snake.moveUp();
+    }
+    if(e.code === 'ArrowDown'){
+        console.log('strzałka w dół');
+        snake.moveDown();
+    }
+});
